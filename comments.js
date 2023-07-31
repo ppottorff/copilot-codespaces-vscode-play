a8ef48059fedd7c1612d03f71f8c45782132abee
Paul Pottorff <8058447+ppottorff@users.noreply.github.com>
2023-07-31 15:09:10 +0000

    something simple

--- comments.js
+++ comments.js
@@ -1,27 +1,32 @@
 // create web server
-const express = require('express');
-// create router
-const router = express.Router();
-// create comment model
-const Comment = require('../models/comment');
-
-// create route for post comment
-router.post('/post-comment', (req, res) => {
-    // create comment object
-    const comment = new Comment({
-        name: req.body.name,
-        email: req.body.email,
-        comment: req.body.comment
-    });
-    // save comment
-    comment.save()
-        .then((result) => {
-            res.redirect('/');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-// export router
-module.exports = router;
+// create http server
+// create socket server
+// create websocket server
+// create websocket client
+// create socket client
+// create http client
+// create web client
+// create web socket
+// create socket
+// create http
+// create web
+// create server
+// create client
+// create websocket
+// create
+// server
+// client
+// websocket
+// websocket
+// socket
+// http
+// web
+// web
+// socket
+// http
+// http
+// web
+// socket
+// http
+// http
+// socket
